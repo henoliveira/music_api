bedc05baae38e8557dbf352d88b8b793d764f88a
Henrique <henriquecastro@unifei.edu.br>
2022-06-13 02:11:35 -0300

    feat: / route returns array of filenames

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from os import getcwd
+from os import getcwd, walk
+from typing import List, Union
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
@@ -9,7 +10,12 @@ CUR_DIR = getcwd()
 SONGS_DIR = f"{CUR_DIR}/public/songs"
 
 
+@api.get("/")
+def get_songs_names() -> Union[List[str], List]:
+    return next(walk(SONGS_DIR), (None, None, []))[2]
+
+
 @api.get("/{file_name}")
-def get_song(file_name: str):
+def get_song(file_name: str) -> FileResponse:
     path = f"{SONGS_DIR}/{file_name}.mp3"
     return FileResponse(path=path, media_type="audio/mp3")
