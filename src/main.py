a554ff5572bbb21784bd9e8454dc17448f33b0b6
Henrique <henriquecastro@unifei.edu.br>
2022-06-13 10:06:54 -0300

    feat: controllers

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
-from os import getcwd, walk
-from typing import List, Union
+from typing import List
 
-import eyed3
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
+
+from controllers.songsControllers import (
+    get_files_names_list,
+    get_song_name_from_file,
+    get_songs_names_list,
+)
 
 api = FastAPI()
 
-CUR_DIR = getcwd()
-SONGS_DIR = f"{CUR_DIR}/public/songs/"
-
 
 @api.get("/")
-def get_songs_list() -> List[str]:
-    filenames: Union[List[str], List] = next(walk(SONGS_DIR), (None, None, []))[2]
-    return list(map(lambda file: eyed3.load(SONGS_DIR + file).tag.title, filenames))
+async def get_songs_list() -> List[str]:
+    return get_songs_names_list(get_files_names_list())
 
 
-@api.get("/{song}")
-def get_song(song: str) -> FileResponse:
-    path = f"{SONGS_DIR}{song}.mp3"
-    return FileResponse(path=path, media_type="audio/mp3")
+@api.get("/{song_name}")
+async def get_song(song_name: str) -> FileResponse:
+    for file_name in get_files_names_list():
+        song = get_song_name_from_file(file_name)
+        if song.tag.title == song_name:
+            return FileResponse(path=song.path, media_type="audio/mp3")
+    raise HTTPException(status_code=404, detail="Song not found")
+
+
+if __name__ == "__main__":
+    uvicorn.run(api, host="0.0.0.0", port=8000)
