86fc128a2a54449e35fd5e7e1529e2f2683514f4
Henrique <henriquecastro@unifei.edu.br>
2022-06-13 02:43:21 -0300

    refactor: get_all  now return song names

--- src/main.py
+++ src/main.py
@@ -1,21 +1,23 @@
 from os import getcwd, walk
 from typing import List, Union
 
+import eyed3
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
 api = FastAPI()
 
 CUR_DIR = getcwd()
-SONGS_DIR = f"{CUR_DIR}/public/songs"
+SONGS_DIR = f"{CUR_DIR}/public/songs/"
 
 
 @api.get("/")
-def get_songs_names() -> Union[List[str], List]:
-    return next(walk(SONGS_DIR), (None, None, []))[2]
+def get_songs_list() -> List[str]:
+    filenames: Union[List[str], List] = next(walk(SONGS_DIR), (None, None, []))[2]
+    return list(map(lambda file: eyed3.load(SONGS_DIR + file).tag.title, filenames))
 
 
-@api.get("/{file_name}")
-def get_song(file_name: str) -> FileResponse:
-    path = f"{SONGS_DIR}/{file_name}.mp3"
+@api.get("/{song}")
+def get_song(song: str) -> FileResponse:
+    path = f"{SONGS_DIR}{song}.mp3"
     return FileResponse(path=path, media_type="audio/mp3")
