ab4fb467611dc050e10a6dd321160ca49fac7f23
Henrique Oliveira <henriquecastro@unifei.edu.br>
2022-06-15 03:24:17 -0300

    feat/rotas-funcionando
    * [FEAT] rotas da api - faltando apenas crud playlist

--- src/main.py
+++ src/main.py
@@ -1,48 +1,139 @@
-from typing import List
-
+import eyed3
 import uvicorn
-from fastapi import FastAPI, HTTPException, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+import env
 
-from .controllers.songsControllers import (
-    get_files_names_list,
-    get_song_from_file,
-    get_songs_names_list,
-    save_song_file,
-)
+from typing import Dict, List, Any
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from supabase import create_client as create_supabase
+from storage3 import create_client as create_bucket
+
 
 api = FastAPI()
-
-origins = ["*"]
+sb = create_supabase(env.sb_url, env.sb_key)
+bucket = create_bucket(
+    env.sb_storage_url, env.sb_storage_headers, is_async=False
+).from_("songs")
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=True,
 )
 
 
-@api.get("/")
-async def get_songs_list() -> List[str]:
-    return get_songs_names_list(get_files_names_list())
+@api.get("/songs")
+def get_songs() -> Dict[str, Any]:
+    res = dict(sb.table("Songs").select("*").execute())
+    return res["data"]
 
 
 @api.get("/song/{song_name}")
-async def get_song(song_name: str) -> FileResponse:
-    for file_name in get_files_names_list():
-        song = get_song_from_file(file_name)
-        if song.tag.title == song_name:
-            return FileResponse(path=song.path, media_type="audio/mp3")
-    raise HTTPException(status_code=404, detail="Song not found")
+def get_song(song_name: str) -> Dict[str, Any]:
+    res = dict(sb.table("Songs").select("*").eq("title", song_name).execute())
+    return res["data"][0]
+
+
+@api.get("/mp3_by_song_name/{song_name}")
+def get_mp3_by_song_name(song_name: str) -> str:
+    res = dict(sb.table("Songs").select("*").eq("title", song_name).execute())
+    path = res["data"][0]["path"]
+    return bucket.get_public_url(path)
+
+
+@api.get("/artists")
+def get_artists() -> List[str]:
+    res = dict(sb.table("Songs").select("artist").execute())
+    return list(dict.fromkeys(map(lambda item: item["artist"], res["data"])))
+
+
+@api.get("/songs_by_artist/{artist_name}")
+def get_songs_by_artist(artist_name: str) -> Dict[str, Any]:
+    res = dict(sb.table("Songs").select("*").eq("artist", artist_name).execute())
+    return res["data"]
+
+
+@api.get("/albums")
+def get_albums() -> List[str]:
+    res = dict(sb.table("Songs").select("album").execute())
+    return list(dict.fromkeys(map(lambda item: item["album"], res["data"])))
+
+
+@api.get("/songs_by_album/{album_name}")
+def get_songs_by_album(album_name: str) -> Dict[str, Any]:
+    res = dict(sb.table("Songs").select("*").eq("album", album_name).execute())
+    return res["data"]
+
+
+@api.patch("/song")
+def update_song(
+    song_name: str, new_name: str = None, new_artist: str = None, new_album: str = None
+):
+    res = dict(sb.table("Songs").select("*").eq("title", song_name).execute())
+    song_data = res["data"][0]
+    data_to_update = {
+        "path": f"{new_name}.mp3" if new_name else song_data["path"],
+        "title": new_name or song_data["title"],
+        "artist": new_artist or song_data["artist"],
+        "album": new_album or song_data["album"],
+    }
+    if new_name:
+        bucket.move(song_data["path"], f"{new_name}.mp3")
+    res = dict(
+        sb.table("Songs").update(data_to_update).eq("title", song_name).execute()
+    )
+    return res["data"][0]
+
+
+@api.patch("/artist")
+def update_artist(artist_name: str, new_artist_name: str):
+    res = dict(
+        sb.table("Songs")
+        .update({"artist": new_artist_name})
+        .eq("artist", artist_name)
+        .execute()
+    )
+    return res["data"][0]
+
+
+@api.patch("/album")
+def update_album(album_name: str, new_album_name: str):
+    res = dict(
+        sb.table("Songs")
+        .update({"album": new_album_name})
+        .eq("album", album_name)
+        .execute()
+    )
+    return res["data"][0]
 
 
 @api.post("/song")
-async def upload_song(file: UploadFile):
-    save_song_file(file)
-    return file
+def upload_song(path: str):
+    mp3 = eyed3.load(path)
+    song: dict = {
+        "path": f"{mp3.tag.title}.mp3",
+        "title": mp3.tag.title,
+        "artist": mp3.tag.artist,
+        "album": mp3.tag.album,
+    }
+    try:
+        bucket.upload(song["path"], path)
+    except Exception:
+        raise HTTPException(404, f"Failed to upload {song['path']}")
+    try:
+        res = dict(sb.table("Songs").insert(song).execute())
+        return res["data"][0]
+    except Exception:
+        raise HTTPException(201, f"Uploaded {song['path']} in database")
+
+
+@api.delete("/song/{song_name}")
+def delete_song(song_name: str):
+    bucket.remove([f"{song_name}.mp3"])
+    res = dict(sb.table("Songs").delete().eq("title", song_name).execute())
+    return res["data"][0]
 
 
 if __name__ == "__main__":
